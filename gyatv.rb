6604571fb471664a1c3abd95fb8241aa893cf01c
Toshiyuki Masui <masui@pitecan.com>
2019-05-26 08:56:17 +0900

    redirectがうまくいかないのでとりあえず回避

--- gyatv.rb
+++ gyatv.rb
@@ -21,7 +21,23 @@ get '/' do
 end
 
 get '/:name' do |name|
-  redirect "/GyaTV/#{name}"
+  texturl = URI.encode("https://scrapbox.io/api/pages/GyaTV/#{name}/text")
+
+  lines = []
+  begin
+    open(texturl){ |f|
+      lines = f.read.split(/\n/)
+    }
+  rescue
+  end
+  lines.shift
+
+  if lines.length == 0
+    redirect "index.html"
+  else
+    @lines_json = lines.to_json
+    erb :gyatv
+  end
 end
 
 get '/:project/:name' do |project,name|
