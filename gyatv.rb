1ae6e71c69edb3876ece69355366720d2fa05b25
Toshiyuki Masui <masui@pitecan.com>
2019-05-26 10:55:34 +0900

    ...

--- gyatv.rb
+++ gyatv.rb
@@ -21,28 +21,16 @@ get '/' do
 end
 
 get '/:name' do |name|
-  texturl = URI.encode("https://scrapbox.io/api/pages/GyaTV/#{name}/text")
-
-  lines = []
-  begin
-    open(texturl){ |f|
-      lines = f.read.split(/\n/)
-    }
-  rescue
-  end
-  lines.shift
-
-  if lines.length == 0
-    redirect "index.html"
-  else
-    @lines_json = lines.to_json
-    erb :gyatv
-  end
+  display 'GyaTV', name
 end
 
 get '/:project/:name' do |project,name|
-  texturl = URI.encode("https://scrapbox.io/api/pages/#{project}/#{name}/text")
+  display project, name
+end
 
+def display(project,name)
+  texturl = URI.encode("https://scrapbox.io/api/pages/#{project}/#{name}/text")
+  
   lines = []
   begin
     open(texturl){ |f|
