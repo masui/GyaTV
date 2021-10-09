6fff6ac1b39fe5c0973159052b8bd7494f5d35ea
Toshiyuki Masui <masui@pitecan.com>
2021-10-10 08:38:09 +0900

    2021/10/1から、Let'sEncrypt使ってるサイトに接続できなくなったので openurlをやめてcurlで誤魔化すことにしてみた

--- gyatv.rb
+++ gyatv.rb
@@ -5,6 +5,12 @@
 # TODO: リダイレクト
 #
 
+# LetsEncryptのROOT証明書の期限が切れたら古いOSからアクセスできなくなって困った
+# https://beyondjapan.com/blog/2021/10/letsencrypt-dst-root-ca-x3-expiration/
+# 2021/10/1からこういう問題が発生した模様
+# 直し方がわからないのでcurlでしのいでみる
+#
+
 require 'sinatra'
 require 'net/http'
 
@@ -30,12 +36,13 @@ end
 
 def display(project,name)
   texturl = URI.encode("https://scrapbox.io/api/pages/#{project}/#{name}/text")
-  
+
   lines = []
   begin
-    open(texturl){ |f|
-      lines = f.read.split(/\n/)
-    }
+    #open(texturl){ |f|
+    #  lines = f.read.split(/\n/)
+    #}
+    lines = `curl #{texturl}`.split(/\n/)
   rescue
   end
   lines.shift
